fcec7d5a29a40d3c45746a0da9e77a7a371c09fc
alexkononenko <alex.kononenko@xtra-auto.com>
2023-07-11 10:12:08 +0200

    create getCarsById

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Honda', 'Toyota', 'Ford', 'BMW'];
   @Get()
   @HttpCode(200)
   getAllCars() {
-    return ['Honda', 'Toyota'];
+    return this.cars;
+  }
+  @Get(':id')
+  getCarById(@Param('id') id: number) {
+    console.log({ id });
+    return this.cars[id];
   }
 }
