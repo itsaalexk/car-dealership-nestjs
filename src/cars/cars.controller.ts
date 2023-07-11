113e22aa9b4242348bad9ce894301b7e266ec7c7
alexkononenko <alex.kononenko@xtra-auto.com>
2023-07-11 10:21:57 +0200

    add getCarsByName

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, HttpCode, Param } from '@nestjs/common';
+import { Controller, Get, HttpCode, Logger, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
   private cars = ['Honda', 'Toyota', 'Ford', 'BMW'];
+  private readonly logger = new Logger(CarsController.name);
   @Get()
   @HttpCode(200)
   getAllCars() {
@@ -12,5 +13,13 @@ export class CarsController {
   getCarById(@Param('id') id: number) {
     console.log({ id });
     return this.cars[id];
+  }
+  @Get(':name')
+  getCarsByName(@Param('name') name: string) {
+    console.log({ name });
+    if (name === '') {
+      return this.logger.log('No car name provided');
+    }
+    return this.cars.filter((car) => car === name);
   }
 }
